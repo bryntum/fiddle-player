ebf147b24fd95b37973767a22f50f99d47a58d83
Mats Bryntse <mats.dev@bryntum.com>
2021-09-29 22:16:52 +0200

    WIP

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // Tests for Splitter bugs fixed
 // Tests for TutorialPanel
 // Handle strings containing space ' ', highlighting breaks
+// Multi line comments
+// Toast showing "Copied to clipboard"
 
 // On website: Card layout playing multiple examples in a sequence unless user interacts
 // New learning center
@@ -13,11 +15,3 @@
 // Handle new lines entered with Enter key
 // Fix focus when typing (or skip highlighting when typing)
 // Click to continue concept
-
-// DONE
-// OK Media query to flip if screen is narrow
-// OK Extract line numbers to its own column + move checkbox in it too
-// OK Show code line count + current index instead of progress bar
-// OK copy to clipboard icon
-// OK: Managed start with blinking cursor
-// OK: Web component
