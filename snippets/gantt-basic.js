1ce2f5e8c115355e328a47b2e4eb94a96272181b
Mats Bryntse <mats.dev@bryntum.com>
2021-12-16 01:20:58 +0100

    WIP

--- snippets/gantt-basic.js
+++ snippets/gantt-basic.js
@@ -1,25 +1,23 @@
 import Gantt from '../../bryntum-suite/Gantt/lib/Gantt/view/Gantt.js';
 
 const gantt = new Gantt({
-    appendTo     : targetElement,//->27
+    appendTo     : targetElement,//->25
     viewPreset   : 'year',
     columns      : [
         { type : 'name', width : 250 }
     ],
-    tasks        : [
-        {
-            id        : 1,
-            name      : 'Go to Mars',
-            iconCls   : 'b-icon b-fa-rocket',
-            expanded  : true,
-            startDate : '2023-02-01',
-            children  : [
-                { id : 2, name : 'Enter space academy', percentDone : 85, duration : 90 },
-                { id : 3, name : 'Find space suit', percentDone : 50, duration : 30 },
-                { id : 4, name : 'Wait for Elon to call', duration : 100 }
-            ]
-        }
-    ],
+    tasks        : [{
+        id        : 1,
+        name      : 'Go to Mars',
+        iconCls   : 'b-icon b-fa-space-shuttle',
+        expanded  : true,
+        startDate : '2023-02-01',//->20
+        children  : [//->19
+            { id : 2, name : 'Enter austronaut academy', percentDone : 85, duration : 90, iconCls : 'b-icon b-fa-user-graduate' },
+            { id : 3, name : 'Buy space suit', percentDone : 50, duration : 30, iconCls : 'b-icon b-fa-user-astronaut' },
+            { id : 4, name : 'Wait for Elon to call', duration : 100, iconCls : 'b-icon b-fa-phone' }
+        ]
+    }],
     dependencies : [
         { from : 2, to : 3 },
         { from : 3, to : 4 }
