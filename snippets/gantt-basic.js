dd9bfdd7403c5ef969d470dc2bf076122eab9b6d
Mats Bryntse <mats.dev@bryntum.com>
2021-12-17 02:02:49 +0100

    WIP

--- snippets/gantt-basic.js
+++ snippets/gantt-basic.js
@@ -9,13 +9,13 @@ const gantt = new Gantt({
     tasks        : [{
         id        : 1,
         name      : 'Go to Mars',
-        iconCls   : 'b-icon b-fa-space-shuttle',
+        iconCls   : 'b-fa b-fa-space-shuttle',
         expanded  : true,
         startDate : '2030-01-01',//->20
         children  : [//->19
-            { id : 2, name : 'Enter austronaut academy', percentDone : 85, duration : 90, iconCls : 'b-icon b-fa-user-graduate' },
-            { id : 3, name : 'Buy space suit', percentDone : 50, duration : 30, iconCls : 'b-icon b-fa-user-astronaut' },
-            { id : 4, name : 'Wait for Elon to call', duration : 100, iconCls : 'b-icon b-fa-phone' }
+            { id : 2, name : 'Astronaut academy', percentDone : 85, duration : 90, iconCls : 'b-fa b-fa-user-graduate' },
+            { id : 3, name : 'Buy space suit', percentDone : 50, duration : 30, iconCls : 'b-fa b-fa-user-astronaut' },
+            { id : 4, name : 'Wait for Elon`s call', duration : 100, iconCls : 'b-fa b-fa-phone' }
         ]
     }],
     dependencies : [
