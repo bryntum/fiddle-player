744aed4badb1f584bc694c002e9b4d519de546aa
Emil Schutte <emilschutte@gmail.com>
2022-07-23 19:57:50 +0200

    Experiment with different syntax for pauses, fix hiding lines for comment-only lines

--- snippets/grid.js
+++ snippets/grid.js
@@ -2,7 +2,7 @@ import { Grid } from 'https://bryntum.com/dist/grid/build/grid.module.js';
 
 const grid = new Grid({
     appendTo : targetElement, ///->17
-    //$2
+    // ..2
     height   : '100%',
     flex     : 1,
     // Define the columns to show in the grid (you can add / remove programmatically too)
