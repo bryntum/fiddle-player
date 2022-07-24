45633482e7d265d8e4ba9c14ea3d4d0e411584ee
Emil Schutte <emilschutte@gmail.com>
2022-07-24 16:17:16 +0200

    Cleanup, simplify, change "wait" syntax from $2 to ..2, fix vertical layout splitter

--- snippets/grid.js
+++ snippets/grid.js
@@ -1,8 +1,8 @@
 import { Grid } from 'https://bryntum.com/dist/grid/build/grid.module.js';
 
 const grid = new Grid({
-    appendTo : targetElement, ///->17
-    // ..2
+    appendTo : targetElement, // ->17
+    // ...2
     height   : '100%',
     flex     : 1,
     // Define the columns to show in the grid (you can add / remove programmatically too)
@@ -15,6 +15,6 @@ const grid = new Grid({
         }
     ]
 });
-///$2
+// ...2
 // Try uncommenting the line below to load data into the grid
 /// grid.store.data = [{ food : 'Pizza' }, { food : 'Salad' }, { food : 'Burger' }];
