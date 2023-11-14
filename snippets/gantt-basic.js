d92b51fe24bbd04922060f48a67736bf5c0cf2a6
Mats Bryntse <mats.dev@bryntum.com>
2023-11-14 18:13:21 +0100

    WIP

--- snippets/gantt-basic.js
+++ snippets/gantt-basic.js
@@ -1,20 +1,20 @@
 import Gantt from '../../bryntum-suite/Gantt/lib/Gantt/view/Gantt.js';
 
 const gantt = new Gantt({
+    viewPreset   : 'year',
     appendTo     : targetElement,//->25
     columns      : [
         { type : 'name', width : 250 }
     ],
-    viewPreset   : 'year',
     tickSize : 30,
     tasks        : [{//->20
         id        : 1,
         name      : `Go to Mars`,
-        startDate : '2030-01-01',
+        startDate : '2030-03-01',
         expanded  : true,
         children  : [//->19
-            { id : 2, name : 'Astronaut academy', percentDone : 85, duration : 150, iconCls : 'b-fa b-fa-user-graduate' },
-            { id : 3, name : 'Buy space suit', percentDone : 50, duration : 80, iconCls : 'b-fa b-fa-user-astronaut' },
+            { id : 2, name : 'Astronaut academy', percentDone : 85, duration : 300, iconCls : 'b-fa b-fa-user-graduate' },
+            { id : 3, name : 'Make space suit', percentDone : 50, duration : 280, iconCls : 'b-fa b-fa-user-astronaut' },
             { id : 4, name : 'Wait for Elon´s call', duration : 0, iconCls : 'b-fa b-fa-phone' }
         ],
     }],
